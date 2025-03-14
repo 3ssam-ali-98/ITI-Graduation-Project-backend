4ea7bba1a38241a7849883ed0bb45953dc8267e7
70sam <hosammedo528@gmail.com>
2025-03-14 17:02:17 +0200

    users final

--- business_management/urls.py
+++ business_management/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet
 from .views import ClientViewSet
+from .views import UserListCreateView, UserDetailView
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
@@ -9,8 +10,8 @@ router.register(r'clients', ClientViewSet)
 
 # router.register(r'business/(?P<business_id>\d+)/tasks', TaskViewSet, basename='tasks')
 
-
-
 urlpatterns = [
     path('', include(router.urls)),
+    path('users/', UserListCreateView.as_view(), name='user-list-create'),
+	path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
 ]
--- business_management/views.py
+++ business_management/views.py
@@ -5,6 +5,16 @@ from .serializers import TaskSerializer
 from .models import Client
 from .serializers import ClientSerializer
 
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, generics, filters
+from django.shortcuts import get_object_or_404
+from .models import User
+from .serializers import UserSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 # Create your views here.
 
@@ -23,4 +33,49 @@ class TaskViewSet(viewsets.ModelViewSet):
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+
+
+class UserListCreateView(generics.ListCreateAPIView):
+	queryset = User.objects.filter(is_superuser=False, is_staff=False)
+	serializer_class = UserSerializer
+	filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+	filterset_fields = ['user_type']
+	search_fields = ['username']
+
+	def perform_create(self, serializer):
+		serializer.save(is_superuser=False, is_staff=False)
+
+
+class UserDetailView(APIView):
+	def get(self, request, pk):
+		user = get_object_or_404(User, pk=pk)
+		serializer = UserSerializer(user)
+		return Response(serializer.data)
+
+	def put(self, request, pk):
+		user = get_object_or_404(User, pk=pk)
+		data = request.data.copy()
+		data.pop("is_superuser", None)
+		data.pop("is_staff", None)
+		serializer = UserSerializer(user, data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_200_OK)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+	def patch(self, request, pk):
+		user = get_object_or_404(User, pk=pk)
+		data = request.data.copy()
+		data.pop("is_superuser", None)
+		data.pop("is_staff", None)
+		serializer = UserSerializer(user, data=data, partial=True)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+	def delete(self, request, pk):
+		user = get_object_or_404(User, pk=pk)
+		user.delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)
 
